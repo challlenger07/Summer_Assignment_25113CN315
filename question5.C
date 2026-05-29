382761e8ccaa3a40d8c345b00f148640a646e0c8
ARAADHY SINGH <challlenger07@gmail.com>
2026-05-29 23:28:14 +0530

    Add files via upload

---
+++ question5.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n ,sum=0,rem;
+    printf("enter number whose sum is to be printed:");
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        rem=n%10;
+        sum=sum+rem;
+        n=n/10;
+        
+    }
+    printf("sum of digit is %d",sum);
+    return 0;
+
+}
